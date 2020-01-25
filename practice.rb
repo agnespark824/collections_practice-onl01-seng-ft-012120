86dd86dfad174ebbe1869691a8593e632a064b16
Agnes Park <agnespark824@gmail.com>
2020-01-25 19:36:19 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,11 +1,2 @@
-def add_s(array)
-  array.collect do |string|
-    if array.index(string) == 1
-      string
-    else 
-      string + "s"
-    end
-  end
-end
 
 [1,2].each_with_index.collect{|element, index| }
